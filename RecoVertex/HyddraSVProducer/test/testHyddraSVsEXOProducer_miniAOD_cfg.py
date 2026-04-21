9f0d6fcd8cb6e3867382c5b2258938d04724d2e3
Andrés J Abreu <andres.abreu@ku.edu>
2026-04-21 14:49:52 -0500

    Fixed Build file errors

--- RecoVertex/HyddraSVProducer/test/testHyddraSVsEXOProducer_miniAOD_cfg.py
+++ RecoVertex/HyddraSVProducer/test/testHyddraSVsEXOProducer_miniAOD_cfg.py
@@ -21,6 +21,7 @@ process.source = cms.Source("PoolSource",
     fileNames=cms.untracked.vstring(options.inputFiles or ['file:input.root']))
 
 process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.Geometry.GeometryRecoDB_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
 
